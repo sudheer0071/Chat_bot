2ac8d7c92a8fe9fa9f9f40569b55c36b0d289041
yashsawkar <sawkaryash257@gmail.com>
2022-10-23 00:48:30 +0530

    Bot design requirement

--- usermessage.py
+++ usermessage.py
@@ -1,12 +1,22 @@
 ## using this Python file to create a voice recognition 
 
 import time as tm
+import datetime as dt
 
 
 def info():
-     syn = "This project is UNDER CONSTUCTION  \n " + tm.ctime() + "\nthis project is about creating \na voice recognition program. \n\n"
+     syn = "This project is UNDER CONSTUCTION  \n " + tm.ctime() + "\nthis project is about creating \na message reply. \n\n"
      for i in syn:
           print(i,end="")
-          tm.sleep(0.005)
+          tm.sleep(0.008)
           
 info()
+
+def printi(syntax):
+     for i in syntax:
+          print(i,end="")
+          tm.sleep(0.016)
+          
+
+     
+
