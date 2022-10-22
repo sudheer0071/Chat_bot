e58119e024ed03290f928a1a677f5978b80c6265
yashsawkar <sawkaryash257@gmail.com>
2022-10-23 03:19:06 +0530

    Learning tensorflow

---
+++ chatbut.py
@@ -0,0 +1,10 @@
+
+import random as rd                           ## to make random decisions
+import numpy as np                            ## array system made easier with numpy
+import json                                   ## to bring javascript function into python
+import pickle                                 ## to serialize and deserialize object struct
+import nltk                                   ## Natural langauge tool kit
+from nltk.stem import WordNetLemmatizer       ## [ working, works, worked ] --> work
+
+
+
