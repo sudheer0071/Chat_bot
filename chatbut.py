e0558a31c4c004e5e5f7cfacc6f7e6b8b94c7833
yashsawkar <sawkaryash257@gmail.com>
2022-10-23 22:57:35 +0530

    tensorflow fault

--- chatbut.py
+++ chatbut.py
@@ -1,19 +1,17 @@
 
-import random as rd                           ## to make random decisions
-import numpy as np                            ## array system made easier with numpy
-import json                                   ## to bring javascript function into python
-import pickle                                 ## to serialize and deserialize object struct
-# import nltk                                   ## Natural langauge tool kit
-# from nltk.stem import WordNetLemmatizer       ## [ working, works, worked ] --> work
+
+# importing tensorflow
 import tensorflow as tf
 
-node1 = tf.constant(6,dtype=tf.int32)
-node2 = tf.constant(7,dtype=tf.int32)
-sumnode = tf.add(node1,node2)
+# creating nodes in computation graph
+node1 = tf.constant(3, dtype=tf.int32)
+node2 = tf.constant(5, dtype=tf.int32)
+node3 = tf.add(node1, node2)
 
-session = tf.compat.v1.Session()
+# create tensorflow session object
+sess = tf.compat.v1.Session()
 
-print(session.run(sumnode))
-
-session.close()
-
+# evaluating node3 and printing the result
+print("sum of node1 and node2 is :",sess.run(node3))
+# closing the session
+sess.close()
