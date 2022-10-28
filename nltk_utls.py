a4926c8dc72fd9ddd10c79061dc89464609edc53
yashsawkar <sawkaryash257@gmail.com>
2022-10-28 23:27:44 +0530

    created the function for tokenisation
    also created a array which will be used to train the bot

--- nltk_utls.py
+++ nltk_utls.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -9,16 +10,29 @@ def tokenize(sentence):
 def stem(word):
      return stemmer.stem(word.lower())
 
-def bag_of_words(tokenised_sentence,all_words):
-     pass
+
+## matching with token words which are already stored to all words
+
+def bag_of_words(token_sent,all_words):
+     token_sent = [stem(w) for w in token_sent]             # token words simplified
+     bag = np.zeros(len(all_words), dtype= np.float32)      # intiallised all to zeros
+     for idx, w in enumerate(all_words):                    # addressing natural no to words in sentence
+          if w in token_sent:                               # since all are intiallised to zero
+               bag[idx] = 1.0                               # required token are intiallaed with 1 value
+          
+     return bag
 
 
-a = "How long does shiping take?"
-print(a)
-a = tokenize(a)
-print(a)
+
+## testing the function
+sentence = ["hello","how"," are","you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+print(bag_of_words(sentence, words))
 
 
-listie = ["organize", "organizes", "organizing"]
-stemmed_words = [stem(i) for i in listie]
-print(stemmed_words)
+     
+
+
+
+
+
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 
 
 import json
-from nltk_utls import tokenize, stem, bag_of_words
+from nltk_utls import tokenize, stem, bag_of_words  # all required function for nltk is created in this file 
+import numpy as np
 
 
 
@@ -12,6 +13,7 @@ all_words = []
 tags = []
 xy = []
 
+# bring the required elements in list
 for intent in intents['intents']:
      tag = intent['tag']
      tags.append(tag)
@@ -21,6 +23,31 @@ for intent in intents['intents']:
           xy.append((w,tag))
      
 ignore_words = ['?','!',',','.']
-print(all_words)
+
+# Bring the complicated text to its stem state
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+X_train = []
+y_train = []
+
+
+for (tokenised_sentence, tags) in xy:
+     bag  = bag_of_words(tokenised_sentence, all_words)
+     X_train.append(bag)
+     
+     label = tags.index(tags)                # indexing the tags
+     y_train.append(label)
+     
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+     
+
+
+
+
+
           
 
